08b29a3f4c56af133920c8758d8e681d382d4209
Brune <abrune.osteo@gmail.com>
2022-11-09 10:59:30 +0100

    commit changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('btn');
+let deckId
 
 btn.addEventListener('click', handleClick)
 
@@ -6,47 +7,12 @@ function handleClick() {
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
 	.then(response => response.json())
 	.then(data => {
-		console.log(data)
+		deckId = data.deck_id
 	})
 }
 
-// setTimeout(callback, 2000)
+setTimeout(callback, 2000)
 
-// function callback() {
-// 	console.log("I finaly ran")
-// }
-
-const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
-]
-
-// const peopleWithPets = people.filter(person => person.hasPet);
-// console.log(peopleWithPets)
-
-// function filterArray(array, callback) {
-//     const resultingArray = []
-//     // Write your filtering logic here
-//     for(let item of array) {
-//        const shouldBeIncluded = callback(item)
-// 	   resultingArray.push(shouldBeIncluded)
-//     }
-// 	return resultingArray
-// }
-
-const lol = [5, 6]
-
-function lolfunction(l, callback) {
-    const result = []
-    for(item of l) {
-        const shouldBeIncluded = callback(item)
-        result.push(shouldBeIncluded)
-    }
+function callback() {
+	console.log("I finaly ran")
 }
-const letsgo = lolfunction(lol, function(number) {
-    return number * 2
-})
-
-console.log(letsgo)
