c089e11b33b46db199599edee1c59636ca01980f
Brune <abrune.osteo@gmail.com>
2022-11-10 09:10:17 +0100

    commit changes

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const btn = document.getElementById('btn');
+const drawBtn = document.getElementById('draw-btn');
+const demo = document.getElementById('cards');
 let deckId
+drawBtn.disabled = true
 
+drawBtn.addEventListener('click', draw2Cards)
 btn.addEventListener('click', handleClick)
 
 function handleClick() {
+	drawBtn.disabled = false
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
 	.then(response => response.json())
 	.then(data => {
 		deckId = data.deck_id
+		console.log(deckId)
 	})
 }
 
-setTimeout(callback, 2000)
-
-function callback() {
-	console.log("I finaly ran")
+function draw2Cards() {
+	drawBtn.disabled = true
+	fetch(` https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data.cards)
+		demo.innerHTML = `
+		<img src=${data.cards[0].image}></img>
+		<img src=${data.cards[1].image}></img>
+		`
+		
+	})
 }
+
