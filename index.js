6b8a6cb72a84f2b4d19c1ad023d9aa4918156a92
Brune <abrune.osteo@gmail.com>
2022-11-16 15:33:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,26 @@ function draw2Cards() {
 	})
 }
 
+function determineCardWinner(card1, card2) {
+	let valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+	const card1ValueIndex = valueOptions.indexOf(card1.value)
+	const card2ValueIndex = valueOptions.indexOf(card2.value)
+	if (card1ValueIndex === card2ValueIndex) {
+		console.log("Draw")
+	} else if (card1ValueIndex > card2ValueIndex) {
+		console.log("Player 1 win")
+	} else if (card1ValueIndex < card2ValueIndex) {
+		console.log("Player 2 win")
+	}
+
+}
+
+const card1Obj = {
+	value: "7"
+}
+
+const card2Obj = {
+	value: "KING"
+}
+
+determineCardWinner(card1Obj, card2Obj)
